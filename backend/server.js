ee092f762eb9870d1914797f1a5f76f7b0a78be1
thijsBoet <m.boet@chello.nl>
2023-05-25 11:25:58 +0200

    created seed and destroy db file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
-dotenv.config();
-
+dotenv.config(); // connect to MongoDb
+connectDB();
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -11,15 +13,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
-});
 
 app.listen(port, () => { 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${port}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${port}`.inverse.green)
 });
