e1c782dfe7dfb6e2faee257920fdf663ba12f43d
thijsBoet <m.boet@chello.nl>
2023-06-05 15:16:35 +0200

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,20 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	})
+}
+
 app.use(notFound);
 app.use(errorHandler);
-
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
 
 app.listen(port, () => {
 	console.log(
